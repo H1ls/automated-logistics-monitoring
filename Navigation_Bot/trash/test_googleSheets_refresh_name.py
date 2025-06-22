b3136f22eb69d9a82025cbf5e7c243a3008dee4a
H1ls <rnaniski@yandex.ru>
2025-06-22 14:13:35 +0300

    Удаление устаревших файлов

--- Navigation_Bot/trash/test_googleSheets_refresh_name.py
+++
@@ -1,6 +0,0 @@
-from Navigation_Bot.googleSheetsManager import GoogleSheetsManager
-
-sheets_manager = GoogleSheetsManager()
-
-INPUT_FILEPATH = "config/selected_data.json"
-sheets_manager.refresh_name(sheets_manager.load_data(), INPUT_FILEPATH)
--- Navigation_Bot/trash/test_gui.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
-import sys
-
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('Тестовое окно')
-window.setGeometry(100, 100, 400, 300)
-window.show()
-sys.exit(app.exec())
